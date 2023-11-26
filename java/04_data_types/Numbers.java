24edfc0acdae4f19070b861c874beedfc1873ba8
locchuong <locchuong123@gmail.com>
2023-11-26 07:40:16 +0700

    update

---
+++ java/04_data_types/Numbers.java
@@ -0,0 +1,7 @@
+public class Numbers{
+    public static void main(String[] args){
+        byte myNum = 100;
+        System.out.print("boolean myNum=");
+        System.out.println(myNum);
+    }
+}
